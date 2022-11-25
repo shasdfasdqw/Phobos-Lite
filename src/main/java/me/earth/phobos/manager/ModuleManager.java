4d575ab2ce989ac0abbec00d15791b90ac1bd888
shasdfasdqw <104833655+shasdfasdqw@users.noreply.github.com>
2022-11-25 21:24:16 +0300

    Update ModuleManager.java

--- src/main/java/me/earth/phobos/manager/ModuleManager.java
+++ src/main/java/me/earth/phobos/manager/ModuleManager.java
@@ -183,7 +183,6 @@ public class ModuleManager
         this.moduleColorMap.put(this.getModuleByClass(ElytraFlight.class), new Color(55, 161, 201));
         this.moduleColorMap.put(this.getModuleByClass(Flight.class), new Color(186, 164, 178));
         this.moduleColorMap.put(this.getModuleByClass(HoleTP.class), new Color(68, 178, 142));
-        this.moduleColorMap.put(this.getModuleByClass(IceSpeed.class), new Color(33, 193, 247));
         this.moduleColorMap.put(this.getModuleByClass(LongJump.class), new Color(228, 27, 213));
         this.moduleColorMap.put(this.getModuleByClass(NoFall.class), new Color(61, 204, 78));
         this.moduleColorMap.put(this.getModuleByClass(NoSlowDown.class), new Color(61, 204, 78));
