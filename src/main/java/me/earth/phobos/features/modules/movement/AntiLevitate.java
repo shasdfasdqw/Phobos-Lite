b94c58330529b6b347c9690c6fc50ebfc29bbf1d
shasdfasdqw <104833655+shasdfasdqw@users.noreply.github.com>
2022-11-25 22:37:42 +0300

    Update AntiLevitate.java

--- src/main/java/me/earth/phobos/features/modules/movement/AntiLevitate.java
+++ src/main/java/me/earth/phobos/features/modules/movement/AntiLevitate.java
@@ -5,16 +5,16 @@ import net.minecraft.potion.Potion;
 
 import java.util.Objects;
 
-public class AntiLevitate
+public class AntiWeakness
         extends Module {
     public AntiLevitate() {
-        super("AntiLevitate", "Removes shulker levitation", Module.Category.MOVEMENT, false, false, false);
+        super("AntiWeakness", "Only Client side lol", Module.Category.MOVEMENT, false, false, false);
     }
 
     @Override
     public void onUpdate() {
-        if (AntiLevitate.mc.player.isPotionActive(Objects.requireNonNull(Potion.getPotionFromResourceLocation("levitation")))) {
-            AntiLevitate.mc.player.removeActivePotionEffect(Potion.getPotionFromResourceLocation("levitation"));
+        if (AntiLevitate.mc.player.isPotionActive(Objects.requireNonNull(Potion.getPotionFromResourceLocation("weakness")))) {
+            AntiLevitate.mc.player.removeActivePotionEffect(Potion.getPotionFromResourceLocation("weakness"));
         }
     }
 }
